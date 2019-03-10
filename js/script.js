d3bc43a5973f3f39b0fa07c5d8fa1d919a41a103
qwertymol <strajcko2@gmail.com>
2019-03-10 16:35:38 +0300

    refactor sources

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+function update(timeDelta) {
+
+}
+
+function render() {
+
+}
+
+var lastTime;
+function main() {
+    var now = Date.now();
+    var dt = (now - lastTime) / 1000.0;
+
+    update(dt);
+    render();
+
+    lastTime = now;
+    requestAnimFrame(main);
+};
+
+function init() {
+    lastTime = Date.now();
+    main();
+}
+
+resources.load([
+    'img/sprites.png',
+    'img/terrain.png'
+]);
+resources.onReady(init);
